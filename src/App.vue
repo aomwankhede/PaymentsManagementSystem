de8d477b734bccf8e6bc36584bf6efd77d0a437e
aom <wankhede.aom@gmail.com>
2025-09-19 14:44:56 +0530

    tailwind setup

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link>
+      <div>
+        <p class="text-blue-600 text-xl font-bold">
+          Hello Tailwind in Vue 3
+        </p>
+      </div>
+    <!-- <router-link to="/">Home</router-link> -->
   </nav>
-  <router-view/>
+  <!-- <router-view/> -->
 </template>
 
 <style>
